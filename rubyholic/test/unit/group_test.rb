a6084a77983f3d8198b5a0d7b7d608823b42487b
beth <lunsford_elizabeth@elizabeth-lunsfords-macbook.local>
2009-03-01 13:20:11 -0800

    test git again

--- rubyholic/test/unit/group_test.rb
+++ rubyholic/test/unit/group_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  end
+   test "validates presence of name" do
+    group = Group.new
+    assert ! group.valid?
+    assert group.errors.on(:name)
+  end
+ 
+  test "name must be unique" do
+    group = Group.new
+    group.name = groups(:one).name
+    assert ! group.valid?
+    assert group.errors.on(:name)
   end
 end
